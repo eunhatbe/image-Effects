9304ffefd5a9dc533b8822cf9f6053ba3394e151
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2023-01-28 01:34:38 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,40 @@
 import time
 import random
+from enum import Enum
 
 import cv2
 
-cap = cv2.VideoCapture()
+# viedo
+# cap = cv2.VideoCapture()
+
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+class InputKey(Enum):
+    ESC = 27
+    SAVE = 's'
+
+def wait_key():
+    input_key = cv2.waitKey(0)
+
+    if input_key == InputKey.ESC.value:
+        cv2.destroyAllWindows()
+    elif input_key == ord(InputKey.SAVE.value):  # 's' key
+        cv2.imwrite('pencilcity.png', img)
+        cv2.destroyAllWindows()
+
+
+img = cv2.imread('city.png', cv2.IMREAD_COLOR)
+img = cv2.GaussianBlur(img, ksize=(9, 9), sigmaX=0)
+
+gray, color = cv2.pencilSketch(img, sigma_s=60, sigma_r=0.05, shade_factor=0.015)
+
+# cv2.imshow('Original', img)
+cv2.imshow('gray', gray)
+
+wait_key()
+
+
+
+
 
