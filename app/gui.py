1c279e12d1bf5f2361c66cfa006dfe4aaab961a0
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2023-02-18 14:28:23 +0900

    그림 그리기 1차 기능

--- app/gui.py
+++ app/gui.py
@@ -1,10 +1,12 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, qApp, QFileDialog, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, qApp, QFileDialog, QLabel, QVBoxLayout, \
+    QMessageBox
 from PyQt5.QtGui import QPixmap
 
-import drawing
+import cv2
+
 
 class App(QMainWindow):
     x = 400
@@ -26,31 +28,41 @@ class App(QMainWindow):
         self.resize(App.width, App.height)
 
         # 이미지 영역 세팅
-        self.img_label = QLabel("Test",self)
-        self.img_label.move(30, 30)
-        self.img_label.resize(400, 20)   # 길이, y좌표
+        self.img_label = QLabel(self)
+        self.img_label.move(200, 50)      # 이미지 영역 좌표
+        self.img_label.resize(500, 500)   # 이미지 크기
 
         # 메뉴바 세팅
         self.menu_bar = self.menuBar()
         self.menu_bar.setNativeMenuBar(False)
 
-        # file menu 액션
-        self.new_action = QAction("파일 불러오기")
-        self.new_action.triggered.connect(self.file_load_action)
+        # file menu event
+        self.file_action = QAction("파일 불러오기")
+        self.file_action.triggered.connect(self.file_load_action)
 
         self.quit_action = QAction("종료")
         self.quit_action.triggered.connect(qApp.quit) # 종료
 
         # file menu 생성
         file_menu = self.menu_bar.addMenu("파일")
-        file_menu.addAction(self.new_action)
-        file_menu.addSeparator()
+        file_menu.addAction(self.file_action)
+        file_menu.addSeparator()            # 경계선 생성
         file_menu.addAction(self.quit_action)
+
+
+        # action menu event
+        self.pencil_img_action = QAction("연필 모드")
+        # self.pencil_img_action.triggered.connect(lambda: pencil_draw(self.img_url)) # 연필 그리기 액션 테스트 코드
+        self.pencil_img_action.triggered.connect(self.pencil_draw)  # 연필 그리기 액션
 
         # action menu 생성
         action_menu = self.menu_bar.addMenu("실행")
+        action_menu.addAction(self.pencil_img_action)
+        # action_menu.addSeparator()    경계선 생성
+
 
         self.show()
+
 
     # 파일 로드
     def file_load_action(self):
@@ -59,15 +71,33 @@ class App(QMainWindow):
         self.draw_img()                             # 이미지 띄우기
         self.show_url(fname)                        # Test code
 
+
     # 파일을 선택하면 이미지를 그림
     def draw_img(self):
         if self.img_url:
             pixmap = QPixmap(self.img_url)
             # 윈도우 비율에 맞게 이미지 크기 조정
             self.img_label.setPixmap(QPixmap(pixmap).scaled(self.width,self.height, Qt.KeepAspectRatio))
-            self.img_label.resize(400,400)
+            # self.img_label.resize(400,400)
 
 
     def show_url(self, fname):
         # self.url_label.setText(self.img_url)
         pass
+
+    # 연필 그리기 기능
+    def pencil_draw(self):
+        # 이미지 선택 판별
+        '''
+        todo
+        이미지를 새로운 창에 그리는 것이 아닌 원래 있던 이미지 영역에 표시하기.
+        '''
+        if self.img_url:
+            img = cv2.imread(self.img_url, cv2.IMREAD_COLOR)
+            img = cv2.GaussianBlur(img, ksize=(9, 9), sigmaX=0)
+            gray, color = cv2.pencilSketch(img, sigma_s=60, sigma_r=0.05, shade_factor=0.015)
+            cv2.imshow('gray', gray)
+
+
+        else:
+            QMessageBox.about(self,"error","이미지를 선택해주세요")
