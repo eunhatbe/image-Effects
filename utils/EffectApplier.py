312bc3f8ca7ab04c75486d7a4be4a35387f66659
Milkis2022 <101296387+eunhatbe@users.noreply.github.com>
2023-06-07 20:10:52 +0900

    test

--- utils/EffectApplier.py
+++ utils/EffectApplier.py
@@ -2,8 +2,20 @@
 
 
 class EffectApplier:
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            print("create object")
+            cls._instance = super().__new__(cls)
+
+        return cls._instance
+
     def __init__(self) -> None:
         pass
 
     def render(self) -> None:
         pass
+
+
+if __name__ == "__main__":
+    pass
